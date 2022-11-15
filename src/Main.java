936426a352d70f20048f129ddacb2538d7177f48
Leonardo Teixeira <lpt.2000@hotmail.com>
2022-11-15 10:25:32 +0000

    2 exercise commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
     Ticket newTicket= new Ticket();
     newTicket.getPassengerInfo();
+        System.out.println(Rental.getRentalCost(0));
+        System.out.println(Rental.getRentalCost(1));
+        System.out.println(Rental.getRentalCost(2));
+        System.out.println(Rental.getRentalCost(3));
+        System.out.println(Rental.getRentalCost(4));
+        System.out.println(Rental.getRentalCost(5));
+        System.out.println(Rental.getRentalCost(6));
+        System.out.println(Rental.getRentalCost(7));
+        System.out.println(Rental.getRentalCost(8));
     }
 }
