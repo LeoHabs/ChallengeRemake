3624acca627ea22070733391c7dc01a25b20bbcc
Leonardo Teixeira <lpt.2000@hotmail.com>
2022-11-15 10:22:20 +0000

    2 exercise commit

--- src/Rental.java
+++ src/Rental.java
@@ -1,9 +1,12 @@
 public class Rental {
 
-    public static void getRentalCost(int days){
-        switch (days){
-            case 1:
-                days<
+    public static double getRentalCost(int days) {
+        if (days < 3) {
+            return days * 35;
+        } else if (days >= 3 && days < 7) {
+            return days * 35 - 10;
+        }else{
+            return days*35 -40;
         }
     }
 }
